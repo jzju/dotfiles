181e99a9e34d3048d79c083cb00996eeec56f12e
Johan Ju <johanj93@gmail.com>
2019-04-29 13:07:17 +0200

    latest

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt install -y \
+build-essential \
+curl \
+python3-pip \
+python3-dev \
+vim \
+wget \
+zsh
+
+sudo pip3 install -U \
+black \
+ipython \
+notebook \
+numpy \
+openpyxl \
+pandas
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mkdir -p ~/.ipython/profile_default/startup/
+ln -s $DIR/.zshrc ~/.zshrc
+ln -s $DIR/.zsh_alias ~/.zsh_alias
+ln -s $DIR/.gitconfig ~/.gitconfig
+ln -s $DIR/50-import.py ~/.ipython/profile_default/startup/50-import.py
+ln -s $DIR/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
+sudo chsh -s /bin/zsh $USER
