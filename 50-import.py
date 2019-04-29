f9011be0ddd7758857a00348e136cf3a7199b0f4
Johan Ju <johanj93@gmail.com>
2019-04-29 13:07:17 +0200

    latest

---
+++ 50-import.py
@@ -0,0 +1,46 @@
+import json
+import math
+import pickle
+import os
+import re
+import shutil
+import warnings
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import traceback as tb
+from collections import defaultdict
+from datetime import datetime as dt
+from time import sleep
+
+warnings.filterwarnings("ignore")
+
+
+def plo(path):
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
+
+def pdu(data, path):
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
+
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+def jlo(path):
+    with open(path, "rb") as f:
+        return json.load(f)
+
+
+def jdu(d, path=None):
+    if path is None:
+        print(json.dumps(d, indent=2, ensure_ascii=0, cls=SetEncoder))
+    else:
+        with open(path, "w") as f:
+            json.dump(d, f, indent=2, ensure_ascii=0, cls=SetEncoder)
---
+++ ipython_config.py
@@ -0,0 +1,4 @@
+c.TerminalInteractiveShell.confirm_exit = False
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
